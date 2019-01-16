b75c64bfce13daccdc27ff135f647763e4f70e60
ilgiz <4ilgiz@gmail.com>
2019-01-16 11:32:17 +0300

    First prog1

---
+++ src/cf/tilgiz/ilgiz2/Main.java
@@ -0,0 +1,12 @@
+package cf.tilgiz.ilgiz2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Number");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        System.out.println("a " + num);
+    }
+}
