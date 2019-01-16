d515d7910099f72aa468ed540ada0a07fe4f25c8
ilgiz <4ilgiz@gmail.com>
2019-01-16 11:54:50 +0300

    First prog2

---
+++ src/cf/tilgiz/ilgiz3/Main.java
@@ -0,0 +1,13 @@
+package cf.tilgiz.ilgiz3;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double a =  Math.random();
+        int num = (int)(a * 10);
+        System.out.println(num);
+
+    }
+}
