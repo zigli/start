31500692e30fdbb68f4a7bd692e69e096effc353
ilgiz <4ilgiz@gmail.com>
2019-01-18 14:37:37 +0300

    Цикл for

---
+++ src/cf/tilgiz/ilgiz4/Main.java
@@ -0,0 +1,12 @@
+package cf.tilgiz.ilgiz4;
+
+public class Main {
+    public static void main(String[] args) {
+        int num = 0;
+        for(;;){
+            if (num == 5) break;
+            System.out.println("Hello");
+            num++;
+        }
+    }
+}
